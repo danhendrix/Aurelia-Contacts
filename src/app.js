8987224f4eeb5aa9798982fca9344fdb20461062
Danny Hendrix <danhendrix@gmail.com>
2017-07-20 12:13:20 -0400

    Add loading bar

--- src/app.js
+++ src/app.js
@@ -1,4 +1,12 @@
+import {inject} from 'aurelia-framework';
+import {WebAPI} from './web-api';
+
+@inject(WebAPI)
 export class App {
+  constructor(api) {
+    this.api = api;
+  }
+  
   configureRouter(config, router) {
     config.title = 'Contacts';
     config.map([
